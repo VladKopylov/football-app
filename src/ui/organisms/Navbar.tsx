57b66a8761691c5c2cd3f8a2c28f7e3017c41e97
kopylov <vd96k@yandex.ua>
2020-07-03 22:08:56 +0300

    create basic navbar

--- src/ui/organisms/Navbar.tsx
+++ src/ui/organisms/Navbar.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return <Nav>Navbar content</Nav>;
+  return (
+    <NavbarContainer>
+      <Logo>Logo</Logo>
+      <AuthSection>
+        <NavLink to='/sign-in'>Войти</NavLink>
+        <NavLink to='/create-account'>Cоздать аккаунт</NavLink>
+      </AuthSection>
+    </NavbarContainer>
+  );
 }
 
-const Nav = styled.nav`
+const NavbarContainer = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   background-color: #4e8098;
-  padding: 25px;
+  height: 10vh;
   box-shadow: -2px -2px 47px 11px rgba(0, 0, 0, 0.5);
+  color: white;
+  font-size: 18px;
 `;
+
+const NavItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  text-decoration: none;
+  color: white;
+
+  &:hover {
+    color: #c2fffe;
+  }
+`;
+
+const Logo = styled.a`
+  font-size: 22px;
+`;
+
+const AuthSection = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+const NavLink = NavItem.withComponent(Link);
 
 export { Navbar };
