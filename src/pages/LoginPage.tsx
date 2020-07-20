fd5532db085b38f2e27fe8ec44c2624082d73d1a
kopylov <vd96k@yandex.ua>
2020-07-20 20:55:24 +0300

    decouple form fields

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, FormProvider, useFormContext } from 'react-hook-form';
 
 import { Input } from '../ui/atoms/Input';
 import { CenterContentTemplate } from '../ui/templates/CenteredContent';
 import { PrimaryButton } from '../ui/atoms/Button';
+
+type FormData = {
+  login: string;
+  password: string;
+};
 
 export function LoginPage(): JSX.Element {
   return (
@@ -14,25 +19,45 @@ export function LoginPage(): JSX.Element {
 }
 
 function LoginForm(): JSX.Element {
-  const { register, handleSubmit, setValue, errors } = useForm();
+  const methods = useForm<FormData>();
+  const { handleSubmit } = methods;
+
   const onSubmit = (data: any) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input
-        placeholder='Email / имя пользователя'
-        name='email'
-        myRef={register}
-      />
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <LoginField />
+        <PasswordField />
+        <PrimaryButton type='submit'>Login</PrimaryButton>
+      </form>
+    </FormProvider>
+  );
+}
+
+function LoginField(): JSX.Element {
+  const { register } = useFormContext();
+
+  return (
+    <Input
+      placeholder='Email / имя пользователя'
+      name='login'
+      myRef={register}
+    />
+  );
+}
+function PasswordField(): JSX.Element {
+  const { register, errors } = useFormContext();
+
+  return (
+    <>
       <Input
         placeholder='Пароль'
         name='password'
         myRef={register({ required: true })}
         type='password'
       />
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <PrimaryButton type='submit'>Login</PrimaryButton>
-    </form>
+      {errors.password && <span>This field is required</span>}
+    </>
   );
 }
