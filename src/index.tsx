70f66e2e5ba72c79069a7b2921410e7c46e76475
kopylov <vd96k@yandex.ua>
2020-07-23 21:54:24 +0300

    change Router -> HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { HashRouter } from 'react-router-dom';
 
 import { App } from './App';
 import { GlobalStyles } from './GlobalStyles';
 
-export const history = createBrowserHistory();
-
 ReactDOM.render(
-  <Router history={history}>
+  <HashRouter>
     <GlobalStyles />
     <App />
-  </Router>,
+  </HashRouter>,
   document.querySelector('#root'),
 );
